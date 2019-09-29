5dacd940ff9c7a32b56755321af16661e9faf2b8
Shihab Khan (ext 1112) <skhan@lxa.on.ca>
2019-09-29 10:51:01 -0400

    fix query worklist and send to pacs by cleaning worklist list data from query

--- tech_forms/urls.py
+++ tech_forms/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('tech_form/', views.tech_form, name='tech_form'),
     path('tech_form_submit/', views.tech_form_submit, name='tech_form_submit'),
+    path('worklist_form_submit/', views.worklist_form_submit,
+         name='worklist_form_submit'),
     path('query_worklist/', views.query_worklist, name='query_worklist'),
     path('admin_login/', auth_views.LoginView.as_view(
         template_name='tech_forms/login.html'), name='login'),
